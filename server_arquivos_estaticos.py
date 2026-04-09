e8006a8069833dc26713214f19ff12569ee704e9
JoseFranciscoDev <josefranciscodevpi@gmail.com>
2026-04-08 21:40:32 -0300

    feat: ajustes

--- server_arquivos_estaticos.py
+++ server_arquivos_estaticos.py
@@ -1,7 +1,7 @@
 import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-# --- TEMPLATES ---
+# TEMPLATES
 INDEX_BEGIN_TEMPLATE = """
 <!DOCTYPE HTML>
 <html lang="pt-br">
@@ -21,9 +21,12 @@ INDEX_END_TEMPLATE = "</ul><hr></body></html>"
 
 NOT_FOUND_TEMPLATE = """
 <!DOCTYPE HTML>
-<html lang="pt-br">
-    <head><meta charset="utf-8"><title>404 Not Found</title></head>
-    <body><h1>404 Not Found</h1><p>O arquivo ou diretório não existe.</p></body>
+<html>
+    <body>
+        <h1>404 Not Found</h1>
+        <p>Error code: 404</p>
+        <p>Message: File not found.</p>
+    </body>
 </html>
 """
 
@@ -34,23 +37,26 @@ MIME_TYPES = {
     "css": "text/css",
     "png": "image/png",
     "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
     "py": "text/x-python",
     "default": "text/html; charset=utf-8"
 }
 
 class MeuHandlerHTTP(BaseHTTPRequestHandler):
     def do_GET(self):
-        # Tradução do caminho da URL para o sistema de arquivos local
         # o path vem como '/diretorio/'
         caminho_local = "." + self.path
         
-        content = b""
+        content = b"" # bytizando a resposta
         status = 200
         content_type = MIME_TYPES["default"]
-
+        if os.path.isdir(caminho_local) and not self.path.endswith("/"):
+            self.send_response(301)
+            self.send_header("Location", self.path + "/")
+            self.end_headers()
+            return 
         try:
             if os.path.isdir(caminho_local):
-
                 # Bora ver se tem um index.html
                 index_path = os.path.join(caminho_local, "index.html")
                 if os.path.exists(index_path):
@@ -58,16 +64,15 @@ class MeuHandlerHTTP(BaseHTTPRequestHandler):
                         content = f.read()
                         content_type = MIME_TYPES["html"]
                 else:
-                    # Gerar Listagem de Diretório
+                    # Montar o djabo do html
                     html = INDEX_BEGIN_TEMPLATE.format(FILE_PATH=self.path)
                     html += FILE_NAME_TEMPLATE.format(RESOURC_PATH="..", FILE_NAME="Voltar")
                     
                     with os.scandir(caminho_local) as entries:
                         for entry in entries:
-                            nome_recurso = entry.name + ("/" if entry.is_dir() else "")
+                            caminho_recurso = entry.name + ("/" if entry.is_dir() else "")
                             nome = entry.name + ("/" if entry.is_dir() else "")
-                            html += FILE_NAME_TEMPLATE.format(FILE_NAME=nome, RESOURC_PATH=nome_recurso)
-                    
+                            html += FILE_NAME_TEMPLATE.format(FILE_NAME=nome, RESOURC_PATH=caminho_recurso)
                     html += INDEX_END_TEMPLATE
                     content = html.encode("utf-8")
                     content_type = MIME_TYPES["html"]
@@ -76,8 +81,8 @@ class MeuHandlerHTTP(BaseHTTPRequestHandler):
                 
                 with open(caminho_local, "rb") as f:
                     content = f.read()
-                    ext = caminho_local.split(".")[-1]
-                    content_type = MIME_TYPES.get(ext, MIME_TYPES["default"])
+                    extensao = caminho_local.split(".")[-1].lower()
+                    content_type = MIME_TYPES.get(extensao, MIME_TYPES["default"])
 
         except FileNotFoundError:
             status = 404
@@ -87,14 +92,14 @@ class MeuHandlerHTTP(BaseHTTPRequestHandler):
             status = 500
             content = f"<h1>Erro Interno: {e}</h1>".encode("utf-8")
 
-        # A gente tem que preparar a resposta com o status, headers e o conteúdo
+
         self.send_response(status)
         self.send_header("Content-Type", content_type)
         self.send_header("Content-Length", len(content))
         self.end_headers()
         self.wfile.write(content)
 
-endereco = ("localhost", 8080)
+endereco = ("0.0.0.0", 8080)
 meu_servidor_http = HTTPServer(endereco, MeuHandlerHTTP)
 try:
     print(f'Servidor rodando em http://{endereco[0]}:{endereco[1]}')
